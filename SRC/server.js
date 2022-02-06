1a0e88afb37059b0e062d01ae6ea761f939851f0
minholee <inth2474@naver.com>
2022-02-06 22:57:48 +0900

    socket

--- SRC/server.js
+++ SRC/server.js
@@ -1,16 +1,26 @@
 import path from 'path';
-const __dirname = path.resolve();
 import express from "express"
+import http from "http";
+import WebSocket, { WebSocketServer } from 'ws';
 
+const __dirname = path.resolve();
 const app = express();
 
 app.set("view engine", "pug");
 app.set("views", __dirname + "/src/views");
 app.use("/public", express.static(__dirname + "/src/public"));
 app.get("/", (req, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
+
 
 const handleListen = () =>console.log(`Listening on http://localhost:3000/`);
-app.listen(3000, handleListen);
 
+const server = http.createServer(app);
+const wss = new WebSocketServer({server});
+server.listen(3000, handleListen);
 
-console.log("hello");
+function handleConnection(socket){
+    console.log(socket);
+}
+
+wss.on("connection", handleConnection);
