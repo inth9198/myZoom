01406d0ad4645baa3c2b2fd78b32ba2ccc8ec7f0
DESKTOP-LAK44MN\이강호 <inth2474@naver.com>
2022-02-08 12:55:09 +0900

    comu

--- SRC/public/js/app.js
+++ SRC/public/js/app.js
@@ -1,17 +1,43 @@
+
+const messageList = document.querySelector("ul");
+const nickFrom = document.querySelector("#nick");
+const messageFrom = document.querySelector("#massage");
 const socket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload){
+    const msg = {type, payload};
+    return JSON.stringify(msg);
+}
 
 socket.addEventListener("open", () => {
     console.log("connected to server!!")
 })
 
 socket.addEventListener("message", (message) => {
-    console.log("j g tis",message.data,"from sv");
+    const li = document.createElement("li");
+    li.innerText = message.data;
+    messageList.append(li);
 })
 
 socket.addEventListener("close", () => {
     console.log("disconected");
 })
+function handleSubmit(event){
+    event.preventDefault();
+    const input = messageFrom.querySelector("input");
+    socket.send(makeMessage("new_message",input.value));
+    const li = document.createElement("li");
+    li.innerText = `You: ${input.value}`;
+    messageList.append(li);
+    input.value = "";
+} 
+function handleNickSubmit(event){
+    event.preventDefault();
+    const input = nickFrom.querySelector("input");
+    socket.send(makeMessage("nickname",input.value));
+    input.value = "";
 
-setTimeout(()=>{
-    socket.send("hello from th bw");
-},1000)
+}
+
+messageFrom.addEventListener("submit", handleSubmit);
+nickFrom.addEventListener("submit", handleNickSubmit)
--- SRC/server.js
+++ SRC/server.js
@@ -17,16 +17,26 @@ const handleListen = () =>console.log(`Listening on http://localhost:3000/`);
 
 const server = http.createServer(app);
 const wss = new WebSocketServer({server});
+const sockets =[];
 
 wss.on("connection", (socket) => {
+    sockets.push(socket);
+    socket["nickname"] = "Anon"
     console.log("to bw")
     socket.on("close", () => {
         console.log("dis from client")
     })
     socket.on("message", (message) => {
-        console.log(message.toString('utf8'))
+        const messageString = message.toString('utf8');
+        const parsed = JSON.parse(messageString);
+        if (parsed.type === "new_message"){
+            sockets.forEach(aso => aso.send(`${socket.nickname}: ${parsed.payload}`));
+        } 
+        else if (parsed.type === "nickname"){
+            socket["nickname"] = parsed.payload;
+        }
+        console.log(message.toString('utf8'));
     })
-    socket.send("hi");
 });
 
 server.listen(3000, handleListen);
