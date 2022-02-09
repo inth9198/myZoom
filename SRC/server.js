ed9793115a6d8b2d5c2e4fa8a16474c963e2281a
DESKTOP-LAK44MN\이강호 <inth2474@naver.com>
2022-02-09 12:41:42 +0900

    message

--- SRC/server.js
+++ SRC/server.js
@@ -1,7 +1,7 @@
-import path from 'path';
 import express from "express"
 import http from "http";
 import { Server } from 'socket.io';
+import path from "path";
 //import WebSocket, { WebSocketServer } from 'ws';
 
 const __dirname = path.resolve();
@@ -18,8 +18,21 @@ const httpServer = http.createServer(app);
 const wsServer = new Server(httpServer);
 
 wsServer.on("connection", (socket) =>{
-    socket.on("enter_room", (msg) => console.log(msg))
-    console.log(socket);
+    socket.onAny((event) =>{
+        console.log(`Socket Event:${event}`);
+    });
+    socket.on("enter_room", (roomName, done) => {
+        socket.join(roomName);
+        done();
+        socket.to(roomName).emit("welcome");
+    });
+    socket.on("disconnectiong", () =>{
+        socket.rooms.forEach(room =>socket.to(room).emit("bye"));
+    })
+    socket.on("new_message", (msg, room, done) => {
+        socket.to(room).emit("new_message", msg);
+        done();
+      });
 })
 /* 
 const wss = new WebSocketServer({server});
@@ -44,5 +57,5 @@ wss.on("connection", (socket) => {
     })
 }); */
 
-const handleListen = () =>console.log(`Listening on http://localhost:3000/`);
+const handleListen = () => console.log(`Listening on http://localhost:3000/`);
 httpServer.listen(3000, handleListen);
